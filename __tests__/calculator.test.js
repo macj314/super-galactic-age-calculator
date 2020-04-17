79609145c3be3ff65887db01440e146af38eceae
unknown <macjay314@gmail.com>
2020-04-17 10:30:44 -0700

    Change expected values to be static numbers instead of an expression. Add failing Mars test.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -5,16 +5,19 @@ describe('Calculator', () => {
   let calc;
 
   beforeEach(() => {
-    //Please enter your age in the line below.
     age = 24;
     calc = new Calculator(age);
   });
 
   test("Accurately calculates age in Mercury years.", () => {
-    expect(calc.mercury(age)).toEqual(.24 / age);
+    expect(calc.mercury(age)).toEqual(100);
   })
 
   test("Accurately calculates age in Venus years.", () => {
-    expect(calc.venus(age)).toEqual(.64 / age);
+    expect(calc.venus(age)).toEqual(37.5);
+  })
+
+  test("Accurately calculates age in Mars years.", () => {
+    expect(calc.mars(age)).toEqual(12.8);
   })
 });
