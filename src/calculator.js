11025df5f7676c921c43ac389bd73b8611b2bac7
unknown <macjay314@gmail.com>
2020-04-17 10:55:17 -0700

    Add Mars method. Refactor previous return statements to round to the nearest tenth.

--- src/calculator.js
+++ src/calculator.js
@@ -5,11 +5,16 @@ export class Calculator{
 
   mercury(){
     this.age = (this.age / .24);
-    return this.age;
+    return Math.round(10*this.age)/10;
   }
 
   venus(){
     this.age = (this.age / .64);
-    return this.age;
+    return Math.round(10*this.age)/10;
+  }
+
+  mars(){
+    this.age = (this.age / 1.88);
+    return Math.round(10*this.age)/10;
   }
 }
