6b1744c426a7666e3157fe08f770683716db583c
unknown <macjay314@gmail.com>
2020-04-17 11:27:35 -0700

    Add life expectancy method.

--- src/calculator.js
+++ src/calculator.js
@@ -22,4 +22,20 @@ export class Calculator{
     this.age = (this.age / 11.86);
     return Math.round(10*this.age)/10;    
   }
+
+
+  lifeExpectancy(lifestyle, healthcare, activity){
+    let expectedAge;
+    let array = [lifestyle, healthcare, activity];
+    for(var i = 0; i < array.length;){
+      if(array[i] === 1){
+        expectedAge += 33;
+      }else if(array[i] === 2){
+        expectedAge += 25;
+      }else if(array[i] === 3){
+        expectedAge += 18;
+      }else expectedAge += 13;      
+    }
+    return expectedAge;
+  }
 }
